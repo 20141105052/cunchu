6881b6f828520439d09625b128e113360da5ad1a
20141105052 <xiaoods3on1@sina.com>
2015-12-02 18:14:52 +0800

    Initial Commit

---
+++ cunchu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cunchu
+//
+//  Created by 20141105052 on 15/12/2.
+//  Copyright (c) 2015年 20141105052. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
